0ebfe1b56dcf095f35eb99e870e55eb44a254000
carlosecleptos <carlossanchez@ucol.mx>
2021-12-16 13:22:07 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {LoginButton} from "./Login";
+import { LogoutButton } from './Logout';
+import {Profile} from "./Profile";
+import { useAuth0 } from '@auth0/auth0-react';
+
+
+function App() {
+  const {isAuthenticated} = useAuth0();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {isAuthenticated ? 
+        <>
+          <Profile/>
+          <LogoutButton/>
+
+        </>
+        :
+        <LoginButton/>
+
+      }
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
